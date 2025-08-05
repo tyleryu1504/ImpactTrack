403730409bd79dbf262d61ca669d207fbdc51389
tyleryu1504 <tyleryu103@gmail.com>
2025-08-05 20:54:25 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,13 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase
 
 // TODO: Replace with your Firebase project's configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBBXlRU8GvhX6TiKA8Xr5odq5l-hEGmW7E",
+  authDomain: "impact-track.firebaseapp.com",
+  projectId: "impact-track",
+  storageBucket: "impact-track.firebasestorage.app",
+  messagingSenderId: "1052527193780",
+  appId: "1:1052527193780:web:e9af5b190ce97d85cc71e1",
+  measurementId: "G-ZQ3QZ8SHY7"
 };
 
 const app = initializeApp(firebaseConfig);
